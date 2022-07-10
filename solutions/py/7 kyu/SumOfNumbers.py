fd2e1be68a4e02eddf8f6883ddae1b4c708ff90e
Kayleigh <41625824+KayleighWasTaken@users.noreply.github.com>
2022-07-10 16:15:26 +0100

    solve more in popular order

---
+++ solutions/py/7 kyu/SumOfNumbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# https://www.codewars.com/kata/55f2b110f61eb01779000053/
+def get_sum(a,b):
+    return sum(range(min(a, b), max(a, b) + 1))
---
+++ solutions/py/8 kyu/GrasshopperSummation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/
+def summation(num):
+    return sum(range(num + 1))
