54ce91ff9d6faab6261ccd386db084fce4355a5f
Kayleigh <41625824+KayleighWasTaken@users.noreply.github.com>
2022-07-10 15:09:15 +0100

    unforget to add link comment

--- solutions/py/5 kyu/1s0sAndWildcards.py
+++ solutions/py/5 kyu/1s0sAndWildcards.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
+# https://www.codewars.com/kata/588f3e0dfa74475a2600002a/
 def possibilities(param):
     return [param.replace("?", "{}").format(*n) for n in [f"{bin(x)[2:].zfill(param.count('?'))}" for x in range(2 ** param.count("?"))]]
