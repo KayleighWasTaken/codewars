6e97ce0e3c326e488929d9efc357a8927c915445
Kayleigh <41625824+KayleighWasTaken@users.noreply.github.com>
2022-07-10 15:14:22 +0100

    comment

--- solutions/py/5 kyu/1s0sAndWildcards.py
+++ solutions/py/5 kyu/1s0sAndWildcards.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 # https://www.codewars.com/kata/588f3e0dfa74475a2600002a/
-def possibilities(param):
+def possibilities(param):  # no imports, no recursion, no newlines, only gaming :sunglasses:
     return [param.replace("?", "{}").format(*n) for n in [f"{bin(x)[2:].zfill(param.count('?'))}" for x in range(2 ** param.count("?"))]]
